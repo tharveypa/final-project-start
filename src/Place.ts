d9f63a4d704a377170d2c053b7e55c49cb8e5b76
SamuraiZach <81524537+SamuraiZach@users.noreply.github.com>
2022-10-26 21:01:17 -0400

    added list of preliminary places framework

---
+++ src/Place.ts
@@ -0,0 +1,11 @@
+export interface Place {
+    Name: string;
+    Country: string;
+    Description: string;
+    Continent: string;
+    Population_Country: number;
+    Average_Tourist: number;
+    onMap: boolean;
+    priority: number;
+    Image: string;
+}
