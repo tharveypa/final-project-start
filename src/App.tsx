252e818acfd65c49e9de5be60213d58775e73278
Justin <jdclav@udel.edu>
2022-10-28 22:20:33 -0400

    added names to page and pushing to test deployment

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,11 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <ul>
+                <li>Justin Clavette</li>
+                <li>Wenhan Ying</li>
+                <li>Junnan Bai</li>
+            </ul>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
