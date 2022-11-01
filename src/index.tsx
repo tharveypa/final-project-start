c186813a1a81127973382653f99ad75b3c8c9932
Max <maxdwatt@udel.edu>
2022-11-01 16:55:20 -0400

    background done with basic buttons

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Board from "./Board";
 import "./index.css";
-import { observe } from "./game";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-observe((picPosition: [number, number]) => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <div
-                style={{
-                    width: "500px",
-                    height: "500px",
-                    border: "1px solid gray"
-                }}
-            >
-                <Board picPosition={picPosition} />
-            </div>
-        </React.StrictMode>,
-        document.getElementById("root")
-    );
-});
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
