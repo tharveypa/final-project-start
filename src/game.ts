3e1e43f520c1621807f843ef2e80e86282ae91b7
Dina Dawood <ddawood@udel.edu>
2022-11-01 16:22:05 -0400

    fixed errors

--- src/game.ts
+++ src/game.ts
@@ -1,10 +1,11 @@
 let picPosition = [0, 0];
-//let observer: ((arg0: number[]) => void) | null = null;
+let observer: ((arg0: number[]) => void) | null = null;
 
 const emitChange = () => {
-    //observer && observer(picPosition);
+    observer && observer(picPosition);
 };
-/*
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const observe = (o: any) => {
     if (observer) {
         throw new Error("Multiple observers not implemented.");
@@ -13,17 +14,16 @@ export const observe = (o: any) => {
     observer = o;
     emitChange();
 };
-*/
+
 export const movePic = (toX: number, toY: number) => {
     picPosition = [toX, toY];
     console.log(picPosition);
     emitChange();
 };
-/*
+
 export const canMovePic = (toX: number, toY: number) => {
     const [x, y] = picPosition;
     const dx = toX - x;
-
+    console.log(toY, y);
     return Math.abs(dx) === 1;
 };
-*/
