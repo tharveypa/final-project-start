be0f61bb1fdd353b4b5b13820b9e047b975bc4db
Patrick Harris <pdharris@udel.edu>
2022-11-01 11:36:39 -0400

    Added Welcome Message but not Displaying

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript,
+                <h1> Welcome to our Website!</h1>
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
