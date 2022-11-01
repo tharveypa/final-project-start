fa6caba021c9da4ce45a1bd5f42ae8ad739e665d
Patrick Harris <pdharris@udel.edu>
2022-11-01 12:08:48 -0400

    Deleted a line of code

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript,
-                <h1> Welcome to our Website!</h1>
+                UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
