2acdd5e392b365009fee01c575e6222f6c6d828b
bladetyr <bladetyr@udel.edu>
2022-11-02 13:34:28 -0400

    functional modals

--- src/components/MakeNote.tsx
+++ src/components/MakeNote.tsx
@@ -5,6 +5,21 @@ import Modal from "react-bootstrap/Modal";
 
 export function MakeNote() {
     const [show, setShow] = useState(false);
+    //FIXME: Use color to set thumbtack color when modal is made
+    const [color, setColor] = useState("red");
+    const colors = ["red", "pink", "orange", "blue", "black"];
+    const [newNote, setNote] = useState();
+
+    const [priority, setPriority] = useState("low");
+    const priorities = ["low", "medium", "high"];
+
+    const colorHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setColor(event.target.value);
+    };
+
+    const priorityHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPriority(event.target.value);
+    };
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -19,7 +34,7 @@ export function MakeNote() {
                 </Modal.Header>
                 <Modal.Body>
                     <Form>
-                        <Form.Group className="makeNote">
+                        <Form.Group className="makeNoteTitle">
                             <Form.Label>Title</Form.Label>
                             <Form.Control
                                 type="textarea"
@@ -27,15 +42,49 @@ export function MakeNote() {
                                 autoFocus
                             />
                         </Form.Group>
-                        <Form.Group className="makeNote">
+                        <Form.Group className="makeNoteDesc">
                             <Form.Label>Description</Form.Label>
                             <Form.Control as="textarea" rows={3} />
+                        </Form.Group>
+                        <Form.Label>Priority</Form.Label>
+                        <Form.Group className="MakeNoteButtons">
+                            {/* Radio buttons and all of their options!! Also disabling eslint because infinite loop */}
+                            {/* eslint-disable-next-line no-extra-parens */}
+                            {priorities.map((p: string) => (
+                                <>
+                                    <Form.Check
+                                        inline
+                                        type="radio"
+                                        id="priorityButtons"
+                                        value={p}
+                                        label={p}
+                                        onChange={priorityHandler}
+                                    />
+                                </>
+                            ))}
+                        </Form.Group>
+                        <Form.Label>Thumbtack Color</Form.Label>
+                        <Form.Group className="MakeNoteButtons">
+                            {/* Radio buttons and all of their options!! */}
+                            {/* eslint-disable-next-line no-extra-parens */}
+                            {colors.map((c: string) => (
+                                <>
+                                    <Form.Check
+                                        inline
+                                        type="radio"
+                                        id="colorButtons"
+                                        value={c}
+                                        label={c}
+                                        onChange={colorHandler}
+                                    />
+                                </>
+                            ))}
                         </Form.Group>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={handleClose}>Close</Button>
-                    <Button onClick={handleClose}>Save Changes</Button>
+                    <Button onClick={handleClose}>Create!</Button>
                 </Modal.Footer>
             </Modal>
         </>
