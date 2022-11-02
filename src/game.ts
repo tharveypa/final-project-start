fa398f46c5f9c6791b3215c7339c94c84ae193ec
SamuraiZach <81524537+SamuraiZach@users.noreply.github.com>
2022-11-02 15:20:08 -0400

    trying to success workflow

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 let picPosition = [0, 0];
 let observer: ((arg0: number[]) => void) | null = null;
 
