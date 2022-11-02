6c4aaece6bb41a53a34534765da52097cbfb5fb6
bladetyr <bladetyr@udel.edu>
2022-11-02 14:05:44 -0400

    modals now generate a task

--- src/components/MakeNote.tsx
+++ src/components/MakeNote.tsx
@@ -5,13 +5,23 @@ import Modal from "react-bootstrap/Modal";
 
 export function MakeNote() {
     const [show, setShow] = useState(false);
+    const [title, setTitle] = useState("");
+    const [description, setDesc] = useState("");
     //FIXME: Use color to set thumbtack color when modal is made
     const [color, setColor] = useState("red");
     const colors = ["red", "pink", "orange", "blue", "black"];
-    const [newNote, setNote] = useState();
+    const [newNote, setNote] = useState({
+        title: "placeholder",
+        description: "",
+        priority: "low",
+        thumbColor: "red"
+    });
 
     const [priority, setPriority] = useState("low");
     const priorities = ["low", "medium", "high"];
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const colorHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         setColor(event.target.value);
@@ -21,13 +31,28 @@ export function MakeNote() {
         setPriority(event.target.value);
     };
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const titleHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.target.value);
+    };
+
+    const descHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setDesc(event.target.value);
+    };
+
+    const createNote = () => {
+        setNote({
+            title: title,
+            description: description,
+            priority: priority,
+            thumbColor: color
+        });
+        handleClose();
+        console.log(newNote);
+    };
 
     return (
         <>
             <Button onClick={handleShow}>Make Note</Button>
-
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>Make a New Note:</Modal.Title>
@@ -39,12 +64,19 @@ export function MakeNote() {
                             <Form.Control
                                 type="textarea"
                                 placeholder="Note title..."
+                                value={title}
+                                onChange={titleHandler}
                                 autoFocus
                             />
                         </Form.Group>
                         <Form.Group className="makeNoteDesc">
                             <Form.Label>Description</Form.Label>
-                            <Form.Control as="textarea" rows={3} />
+                            <Form.Control
+                                as="textarea"
+                                rows={3}
+                                value={description}
+                                onChange={descHandler}
+                            />
                         </Form.Group>
                         <Form.Label>Priority</Form.Label>
                         <Form.Group className="MakeNoteButtons">
@@ -84,7 +116,7 @@ export function MakeNote() {
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={handleClose}>Close</Button>
-                    <Button onClick={handleClose}>Create!</Button>
+                    <Button onClick={createNote}>Create!</Button>
                 </Modal.Footer>
             </Modal>
         </>
