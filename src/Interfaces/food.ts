7fafcf72aa9846cb88259fd8dbd82c2b1effba38
Matt Whalen <mwhalen@udel.edu>
2022-11-03 12:11:17 -0400

    food.ts and foodlist.json

--- src/Interfaces/food.ts
+++ src/Interfaces/food.ts
@@ -1,8 +1,6 @@
 export interface Food {
     name: string;
     calories: number;
-    /** The spoiling rate is how many days it takes for the food item to expire */
-    spoiling_rate: number;
     image_link: string;
     healthy: boolean;
     /** ingredients: string[] */
