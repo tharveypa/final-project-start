834808536f85466609773ff2c9da2ec6de61a09c
emba00 <emba@udel.edu>
2022-11-07 22:38:33 -0500

    should deploy

--- src/components/CarSpace.tsx
+++ src/components/CarSpace.tsx
@@ -1,16 +1,25 @@
-import React from "react";
-import bluecar from "./blue car.png";
+import React, { useState } from "react";
+import RedCar from "./red car.png";
+import BlueCar from "./blue car.png";
+import GreenCar from "./green car.png";
+import { Button } from "react-bootstrap";
 
-type CarSpaceProps = {
+type CarProps = {
     color: string;
-    clean: boolean;
-    tirefill: boolean;
-    windowstate: boolean;
-    shine: boolean;
 };
 
-const CarSpace: React.FC<CarSpaceProps> = () => {
-    return <img src={bluecar} alt="car" />;
+const Car: React.FC<CarProps> = (props) => {
+    const [colorNum, setColorNum] = useState(0);
+    return (
+        <div>
+            {colorNum == 0 && <img src={RedCar} alt="red car" />}
+            {colorNum == 1 && <img src={BlueCar} alt="blue car" />}
+            {colorNum == 2 && <img src={GreenCar} alt="green car" />}
+            <Button onClick={() => setColorNum((colorNum + 1) % 3)}>
+                Change Color (Red/Blue/Green)
+            </Button>
+        </div>
+    );
 };
 
-export default CarSpace;
+export default Car;
