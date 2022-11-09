2f7414bb74535d39e60fb0412b38b254961c9d1f
Justin <jdclav@udel.edu>
2022-11-09 14:41:17 -0500

    changed pan activation key to Shift

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { MouseEvent, MouseEventHandler, useState } from "react";
 import "./App.css";
 import Board from "./Board";
 import GridEdit from "./GridEdit";
 import "./background.css";
 import ImageDownload from "./ImageDownload";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import Pic from "./Pic";
 
 type BoardProps = {
     picPosition: [number, number];
@@ -13,16 +14,35 @@ type BoardProps = {
 const App: React.FC<BoardProps> = (props): JSX.Element => {
     const [xSize, setXSize] = useState<number>(5);
     const [ySize, setYSize] = useState<number>(5);
+    const [pan, setPan] = useState<boolean>(false);
 
     const changeXSize = (x: number) => setXSize(x);
     const changeYSize = (y: number) => setYSize(y);
 
+    /* Working on middle mouse panning
+    const handleMiddleDown = (event: React.MouseEvent) => {
+        if (event.button === 1) {
+            setPan(true);
+        }
+    };
+
+    const handleMiddleUp = (event: React.MouseEvent) => {
+        if (event.button === 1) {
+            setPan(false);
+        }
+    };
+
+     disabled: !pan, 
+
+    onMouseDown={handleMiddleDown}
+    onMouseUp={handleMiddleUp}
+    */
     return (
         <div className="App">
             <header className="App-header">Table Top Map Editor</header>
             <ImageDownload></ImageDownload>
             <GridEdit changeX={changeXSize} changeY={changeYSize}></GridEdit>
-            <TransformWrapper panning={{ activationKeys: ["z"] }}>
+            <TransformWrapper panning={{ activationKeys: ["Shift"] }}>
                 <TransformComponent>
                     <div
                         className="container"
