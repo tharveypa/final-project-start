208cfc8dfd6894716d20adac80be8ae42914b468
Matt Whalen <mwhalen@udel.edu>
2022-11-09 21:59:49 -0500

    matt-dnd fix again

--- src/Board.tsx
+++ src/Board.tsx
@@ -3,16 +3,11 @@ import Pic from "./Pic";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import BoardSquare from "./BoardSquare";
-import pic1 from "./bosun_tally.jpg";
-import { PROTEIN_LIST, FRUIT_LIST } from "./data/foodList";
+//import pic1 from "./bosun_tally.jpg";
+import { PROTEIN_LIST } from "./data/foodList";
 import { Food } from "./Interfaces/food";
 
-const renderPiece = (
-    x: number,
-    y: number,
-    [picX, picY]: [number, number],
-    foodItem: Food
-) => {
+const renderPiece = (x: number, y: number, foodItem: Food) => {
     // if (x === picX && y === picY) {
     //     return (
     //         <div>
@@ -37,10 +32,8 @@ const renderSquare = (
 
     return (
         <div key={i} style={{ width: "50%", height: "100%" }}>
-            <BoardSquare x={x} y={y} foodItem={foodItem}>
-                {i < PROTEIN_LIST.length
-                    ? renderPiece(x, y, picPosition, foodItem)
-                    : null}
+            <BoardSquare x={x} y={y}>
+                {i < PROTEIN_LIST.length ? renderPiece(x, y, foodItem) : null}
             </BoardSquare>
         </div>
     );
@@ -53,7 +46,6 @@ type BoardProps = {
 const Board: React.FC<BoardProps> = (props) => {
     const { picPosition } = props;
     const squares = [];
-    const foodNames: string[] = ["Apple", "Orange", "Done"];
     /**Determines the number of drag-and-drop squares to make */
     for (let i = 0; i < PROTEIN_LIST.length + 1; i++) {
         squares.push(renderSquare(i, picPosition, PROTEIN_LIST[i]));
--- src/FridgeButtons.tsx
+++ src/FridgeButtons.tsx
@@ -1,21 +1,11 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
-// export const ShowFridgeButtons = () => {
-//     function ShowFridgePictures(name: string) {}
-
-//     return (
-//         <div>
-//             <div>
-//                 <Button
-//                     onClick={() => {
-//                         ShowFridgePictures("Protein");
-//                     }}
-//                 >
-//                     Protein
-//                 </Button>
-//             </div>
-//         </div>
-//     );
-// };
-// export default ShowFridgeButtons;
+export const ShowFridgeButtons = () => (
+    /*function ShowFridgePictures(name: string) {}*/ <div>
+        <div>
+            <Button>Protein</Button>
+        </div>
+    </div>
+);
+export default ShowFridgeButtons;
