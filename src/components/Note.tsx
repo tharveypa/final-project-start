9b2b2d65336570e8c71d35a5f5af0a3e363744e3
ydnamjs <ydnamjs@udel.edu>
2022-11-11 12:50:33 -0500

    Added dragging functionality to Notes

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { useDrag } from "react-dnd";
+import { ItemTypes } from "../constants";
 import { Task } from "../interfaces/task";
 
 export function Note({ task, id }: { task: Task; id: number }): JSX.Element {
+    //handles the dragging of the Note
+    const [, drag] = useDrag({
+        //type is note; Type determines where it can be dropped
+        item: { type: ItemTypes.Note, id: id }
+    });
+
     return (
         <div
+            //makes the dragging of the note work
+            ref={drag}
+            //style controls how the note looks
             style={{
                 height: "100%",
                 width: "100%",
                 backgroundColor: "yellow"
-                //position: "relative"
             }}
         >
             Title: {task.title}
