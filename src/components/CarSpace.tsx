0ef57730af05991c4aec4945c8a917c672f68d5a
Andrew Geraci <ajg10000bc@gmail.com>
2022-11-11 14:51:03 -0500

    change background

--- src/components/CarSpace.tsx
+++ src/components/CarSpace.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import RedCar from "./red car.png";
 import BlueCar from "./blue car.png";
 import GreenCar from "./green car.png";
+import Street from "./street.jpg";
+import City from "./city.jpg";
+import Forest from "./forest.jpg";
 import { Button } from "react-bootstrap";
 
 type CarProps = {
@@ -11,15 +14,32 @@ type CarProps = {
 };
 
 const Car: React.FC<CarProps> = (props) => {
+    const backgrounds = [Street, City, Forest];
     const [colorNum, setColorNum] = useState(0);
+    const [backgroundIndex, setBackgroundIndex] = useState(0);
     return (
         <div>
-            {colorNum == 0 && <img src={RedCar} alt="red car" />}
-            {colorNum == 1 && <img src={BlueCar} alt="blue car" />}
-            {colorNum == 2 && <img src={GreenCar} alt="green car" />}
-            <Button onClick={() => setColorNum((colorNum + 1) % 3)}>
-                Change Color (Red/Blue/Green)
-            </Button>
+            <div
+                style={{
+                    backgroundImage: `url(${backgrounds[backgroundIndex]})`
+                }}
+            >
+                {colorNum == 0 && <img src={RedCar} alt="red car" />}
+                {colorNum == 1 && <img src={BlueCar} alt="blue car" />}
+                {colorNum == 2 && <img src={GreenCar} alt="green car" />}
+            </div>
+            <div>
+                <Button onClick={() => setColorNum((colorNum + 1) % 3)}>
+                    Change Color (Red/Blue/Green)
+                </Button>
+                <Button
+                    onClick={() =>
+                        setBackgroundIndex((backgroundIndex + 1) % 3)
+                    }
+                >
+                    Change Background (Street/City/Forest)
+                </Button>
+            </div>
         </div>
     );
 };
