105cfb396f53ee87a5b8fe2a725ab471b8163d60
Grace O'Leary <graceoleary@wifi-roaming-128-4-67-208.wifi.udel.edu>
2022-11-15 17:07:22 -0500

    new folder

--- src/Components/furniture.tsx
+++ src/Components/furniture.tsx
@@ -1,20 +1,70 @@
+import { isCommaListExpression } from "typescript";
+
 export {};
 const furniture = ["Chairs", "Couches", "Tables", "Beds", "Dressers"];
 
 interface Chair {
     type: string;
     size: string;
-    color: string;
+    pattern: string;
+    color: string[];
     isComfy: boolean;
+    picture: string;
 }
 
-const dining: Chair = {
+const chair1 = {
     type: "Dining Room",
     size: "Small",
-    color: "brown",
-    isComfy: false
+    pattern: "Solid",
+    color: ["white"],
+    isComfy: false,
+    picture: "src/Furniture/chair 1.jpg" //chair1.picture the picture will show up
 };
 
+const chair2 = {
+    type: "Living Room",
+    size: "Medium",
+    pattern: "Stripes",
+    color: ["brown", "black", "white"],
+    isComfy: true,
+    picture: "src/Furniture/chair 2.jpg"
+};
+
+const chair3 = {
+    type: "Living Room",
+    size: "Small",
+    pattern: "Zig-Zag",
+    color: ["white", "green"],
+    isComfy: true,
+    picture: "src/Furniture/chair 3.jpg"
+};
+
+const chair4 = {
+    type: "Living Room",
+    size: "Medium",
+    pattern: "Solid",
+    color: ["blue"],
+    isComfy: true,
+    picture: "src/Furniture/chair 4.jpg"
+};
+
+const chair5 = {
+    type: "Bedroom",
+    size: "Medium",
+    pattern: "Stripes",
+    color: ["pink", "white", "light blue"],
+    isComfy: true,
+    picture: "src/Furniture/chair 5.jpg"
+};
+
+const chair6 = {
+    type: "Bedroom",
+    size: "Large",
+    pattern: "Zig-Zag",
+    color: ["Brown", "cream"],
+    isComfy: true,
+    picture: "src/Furniture/chair 6.jpg"
+};
 interface Couch {
     type: string;
     size: string;
