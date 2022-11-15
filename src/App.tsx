45acefe7828b316dd37ffb328e7898c3c10d5645
Justin <jdclav@udel.edu>
2022-11-15 15:59:08 -0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ const App: React.FC = (): JSX.Element => {
     return (
         <div className="App">
             <header className="App-header">
-                Table Top Map Editor for CISC275
+                Tabletop Map Editor for CISC275
             </header>
             <div>Justin Clavette</div>
             <ImageDownload></ImageDownload>
