81f8a5da11b43bec7cd160cbb8c2271f793e9907
emba00 <emba@udel.edu>
2022-11-15 17:55:20 -0500

    car and carspace stuff

--- src/components/CarSpace.tsx
+++ src/components/CarSpace.tsx
@@ -1,47 +1,18 @@
-
-import React, { useState } from "react";
-import RedCar from "./images/red car.png";
-import BlueCar from "./images/blue car.png";
-import GreenCar from "./images/green car.png";
-import Street from "./images/street.jpg";
-import City from "./images/city.jpg";
-import Forest from "./images/forest.jpg";
-import { Button } from "react-bootstrap";
-
+import React from "react";
+import DirtTool from "./Destroy Tools/DirtTool";
+import Car from "./Car";
 
 type CarSpaceProps = {
     DirtPosition: number[];
 };
 
-
-const Car: React.FC<CarProps> = () => {
-    const backgrounds = [Street, City, Forest];
-    const [colorNum, setColorNum] = useState(0);
-    const [backgroundIndex, setBackgroundIndex] = useState(0);
+function renderDestroy(x: number, y: number, [dirtX, dirtY]: number[]) {
+    const isDirtHere = dirtX === x && dirtY === y;
+    const piece = isDirtHere ? <DirtTool /> : null;
     return (
-        <div>
-            <div
-                style={{
-                    backgroundImage: `url(${backgrounds[backgroundIndex]})`
-                }}
-            >
-                {colorNum == 0 && <img src={RedCar} alt="red car" />}
-                {colorNum == 1 && <img src={BlueCar} alt="blue car" />}
-                {colorNum == 2 && <img src={GreenCar} alt="green car" />}
-            </div>
-            <div>
-                <Button onClick={() => setColorNum((colorNum + 1) % 3)}>
-                    Change Color (Red/Blue/Green)
-                </Button>
-                <Button
-                    onClick={() =>
-                        setBackgroundIndex((backgroundIndex + 1) % 3)
-                    }
-                >
-                    Change Background (Street/City/Forest)
-                </Button>
-            </div>
-        </div>
+        <Car color={0} clean={true} window={true} tirefill={true}>
+            {piece}
+        </Car>
     );
 }
 const CarSpace: React.FC<CarSpaceProps> = (props) => {
