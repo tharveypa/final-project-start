f08384b392f2924f139788f2e51a11ea1c6ce663
SamuraiZach <81524537+SamuraiZach@users.noreply.github.com>
2022-11-17 09:48:02 -0500

    Forgot to add PicSquare, althoguht it is not implemented

---
+++ src/PicSquare.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useDrop } from "react-dnd";
+import { ItemTypes } from "./constants";
+import { canMovePic, movePic } from "./game";
+import Overlay from "./Overlay";
+import Square from "./Square";
+
+type BoardSquareProps = {
+    x: number;
+    y: number;
+};
+
+const PicSquare: React.FC<BoardSquareProps> = (props) => {
+    const { x, y, children } = props;
+    const black = false; /*(x + y) % 2 === 1;*/ /*TEST*/
+    const [{ isOver, canDrop }, drop] = useDrop({
+        accept: ItemTypes.PIC,
+        canDrop: () => canMovePic(x, y),
+        drop: () => movePic(x, y),
+        collect: (monitor) => ({
+            isOver: !!monitor.isOver(),
+            canDrop: !!monitor.canDrop()
+        })
+    });
+
+    return (
+        <div
+            ref={drop}
+            style={{ position: "relative", width: "100%", height: "100%" }}
+        ></div>
+    );
+};
+
+export default PicSquare;
