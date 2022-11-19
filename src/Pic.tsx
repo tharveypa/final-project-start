5665910aa0b1dfbecc5a3e87a41804733c2f22e8
junanBai <jnbai@udel.edu>
2022-11-19 17:53:04 -0500

    Delete picture.tsx, scroll.tsx and img.tsx

--- src/Pic.tsx
+++ src/Pic.tsx
@@ -1,7 +1,8 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import { useDrag } from "react-dnd";
 import { ItemTypes } from "./constants";
 import { tileItem } from "./interfaces";
+// import imgList from "./image/allImage";
 // import { images, image1 } from "./image/images";
 // import images from "./image/img";
 // import { BackgroundImage } from "./image/img";
@@ -23,6 +24,12 @@ const Pic: React.FC<ObjectProp> = (props) => {
             isDragging: !!monitor.isDragging
         })
     });
+    // const [index, setIndex] = useState<number>(0);
+    // function CurrentIndex() {
+    //     setIndex(index + 1);
+    // }
+    // let imageAllName = imgList[]
+
     // const [choseImage, SetChoseImage] = useState<string>("./image/cat.jpg");
     // const currentImage = SetChoseImage("./image/cat.jpg" || "./image/dog.jpg");
     // const current = choseImage === currentImage?
@@ -43,49 +50,11 @@ const Pic: React.FC<ObjectProp> = (props) => {
                     {props.tile.color}
                     <img
                         src={require("./bosun_tally.jpg")}
+                        // src = {imgList[index]}
                         width="100%"
                         height="100%"
                     />
                 </div>
-                <div>
-                    {props.tile.color}
-                    <img
-                        src={require("./image/cat.jpg")}
-                        width="100%"
-                        height="100%"
-                    />
-                </div>
-                <div>
-                    {props.tile.color}
-                    <img
-                        src={require("./image/tree.jpg")}
-                        width="100%"
-                        height="100%"
-                    />
-                </div>
-            </div>
-            <div
-                ref={drag}
-                style={{
-                    opacity: isDragging ? 1 : 0.5,
-                    fontSize: 10,
-                    fontWeight: "bold",
-                    cursor: "move",
-                    textAlign: "center"
-                }}
-            >
-                {props.tile.color}
-                <img
-                    src={require("./image/hugeStone.jpg")}
-                    width="100%"
-                    height="100%"
-                />
-                {props.tile.color}
-                <img
-                    src={require("./image/stone.jpg")}
-                    width="100%"
-                    height="100%"
-                />
             </div>
         </Fragment>
     );
