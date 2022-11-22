46b9b08f6ec09ced8733b2908cb230ab3c95dd6c
Kristina <kbalta@udel.edu>
2022-11-22 17:41:36 -0500

    dk

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,28 @@
-/* eslint-disable react/react-in-jsx-scope */
-import { render } from "react-dom";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import React from "react";
+import ReactDOM from "react-dom";
+import Board from "./Board";
+import "./index.css";
+import { observe } from "./game";
+import reportWebVitals from "./reportWebVitals";
 
-function App() {
-    return (
-        <div className="App">
-            <DndProvider backend={HTML5Backend}></DndProvider>
-        </div>
+observe((picPosition: [number, number]) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <div
+                style={{
+                    width: "100%",
+                    height: "500px",
+                    border: "1px solid gray"
+                }}
+            >
+                <Board picPosition={picPosition} />
+            </div>
+        </React.StrictMode>,
+        document.getElementById("root")
     );
-}
+});
 
-const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
