ea14a424335dce77b3e422ca9026332194e9a8fa
bladetyr <bladetyr@udel.edu>
2022-11-24 17:31:28 -0500

    Cards no longer show thumbtack color

--- src/components/CardComp.tsx
+++ src/components/CardComp.tsx
@@ -23,7 +23,6 @@ function CardComp({ task, id }: { task: Task; id: number }): JSX.Element {
                     <Card.Title>{task.title}</Card.Title>
                     <Card.Text>{task.description}</Card.Text>
                     <Card.Text>Priority: {task.priority}</Card.Text>
-                    <Card.Text>Thumbtack Color: {task.thumbColor}</Card.Text>
                     <Card.Text>Assignees: {assi}</Card.Text>
                 </Card.Body>
             </Card>
