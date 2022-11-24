853418ae103cf3c915eff6a1a4471f93a094d7ae
ydnamjs <ydnamjs@udel.edu>
2022-11-23 23:02:46 -0500

    Changed wording of the modal to better reflect what is being done; improved commenting; removed irrelevant code; fixed radio buttons not updating

--- src/components/EditCard.tsx
+++ src/components/EditCard.tsx
@@ -5,55 +5,58 @@ import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 
 export function EditCard({
+    // input parameter from CardList ; is used within editCard so that CardList knows what card is being changed
     id,
-    // input parameter from CardList ; Used for the creating of card so that it gets added to CardList
+    // input parameter from CardList ; Used for the editing of a card so that it gets changed in CardList
     editCard
 }: {
     id: number;
     editCard: (id: number, inTask: Task) => void;
 }): JSX.Element {
+    // used to manage visibility of editCard modal
     const [show, setShow] = useState(false);
-    const [title, setTitle] = useState("");
-    const [description, setDesc] = useState("");
-    //FIXME: Use color to set thumbtack color when modal is made
-    const [color, setColor] = useState("red");
-    const colors = ["red", "pink", "orange", "blue", "black"];
-    const [newNote, setNote] = useState({
-        title: "placeholder",
-        description: "",
-        priority: "low",
-        thumbColor: "red"
-    });
 
+    // state that holds the title the user input
+    const [title, setTitle] = useState("");
+    // state that holds the description the user input
+    const [description, setDesc] = useState("");
+    // state that holds the color the user selected
+    const [color, setColor] = useState("red");
+    // state that holds the priority the user selected
     const [priority, setPriority] = useState("low");
+
+    // list of colors the user can select
+    const colors = ["red", "pink", "orange", "blue", "black"];
+    // list of colors the user can select
     const priorities = ["low", "medium", "high"];
 
+    // function that sets the modal to be hidden
     const handleClose = () => setShow(false);
+    // function that sets the modal to be shown
     const handleShow = () => setShow(true);
 
+    // function that handles a color being selected
     const colorHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         setColor(event.target.value);
     };
 
+    // function that handles a priority being selected
     const priorityHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         setPriority(event.target.value);
     };
 
+    // function that handles a title being typed
     const titleHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         setTitle(event.target.value);
     };
 
+    // function that handles a description being typed
     const descHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         setDesc(event.target.value);
     };
 
-    const createNote = () => {
-        setNote({
-            title: title,
-            description: description,
-            priority: priority,
-            thumbColor: color
-        });
+    // function that handles submitting the edits to the Card to CardList
+    const sendEdit = () => {
         handleClose();
 
         // Calls the function passed in from CardList to add the created card to CardList
@@ -63,19 +66,20 @@ export function EditCard({
             priority: priority,
             thumbColor: color
         });
-        console.log(newNote);
-        //globalNote = newNote;
     };
 
     return (
         <>
+            {/* Button that opens the editCard Modal */}
             <Button onClick={handleShow}>🖉</Button>
+            {/* Modal that facilitates the editing of cards */}
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>Edit Card:</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form>
+                        {/* Text entry box for title */}
                         <Form.Group className="makeNoteTitle">
                             <Form.Label>Title</Form.Label>
                             <Form.Control
@@ -86,6 +90,8 @@ export function EditCard({
                                 autoFocus
                             />
                         </Form.Group>
+
+                        {/* Text entry box for description */}
                         <Form.Group className="makeNoteDesc">
                             <Form.Label>Description</Form.Label>
                             <Form.Control
@@ -96,9 +102,10 @@ export function EditCard({
                                 onChange={descHandler}
                             />
                         </Form.Group>
+
+                        {/* Radio buttons for priority */}
                         <Form.Label>Priority</Form.Label>
                         <Form.Group className="MakeNoteButtons">
-                            {/* Radio buttons and all of their options!! Also disabling eslint because infinite loop */}
                             {/* eslint-disable-next-line no-extra-parens */}
                             {priorities.map((p: string) => (
                                 <>
@@ -109,13 +116,15 @@ export function EditCard({
                                         value={p}
                                         label={p}
                                         onChange={priorityHandler}
+                                        checked={priority === p}
                                     />
                                 </>
                             ))}
                         </Form.Group>
+
+                        {/* Radio buttons for thumbtack color */}
                         <Form.Label>Thumbtack Color</Form.Label>
                         <Form.Group className="MakeNoteButtons">
-                            {/* Radio buttons and all of their options!! */}
                             {/* eslint-disable-next-line no-extra-parens */}
                             {colors.map((c: string) => (
                                 <>
@@ -126,6 +135,7 @@ export function EditCard({
                                         value={c}
                                         label={c}
                                         onChange={colorHandler}
+                                        checked={color === c}
                                     />
                                 </>
                             ))}
@@ -133,8 +143,10 @@ export function EditCard({
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
+                    {/* Button that closes the modal without saving */}
                     <Button onClick={handleClose}>Close</Button>
-                    <Button onClick={createNote}>Create!</Button>
+                    {/* Button that saves changes to the card and closes the modal */}
+                    <Button onClick={sendEdit}>Save</Button>
                 </Modal.Footer>
             </Modal>
         </>
