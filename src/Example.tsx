4d676a7ac0b152b1351f9da6439c9828eb982acd
SamuraiZach <81524537+SamuraiZach@users.noreply.github.com>
2022-11-27 15:19:05 -0500

    modifications

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,11 +1,13 @@
 import type { FC } from "react";
 import React from "react";
 import { useCallback, useState } from "react";
-
+import { Place } from "./Place";
+import sample from "./Places.json";
 import { Container } from "./Container";
 
 export const Example: FC = () => {
     const [hideSourceOnDrag, setHideSourceOnDrag] = useState(true);
+    const PLACES = sample.map((place): Place => ({ ...place }));
     const toggle = useCallback(
         () => setHideSourceOnDrag(!hideSourceOnDrag),
         [hideSourceOnDrag]
@@ -13,19 +15,31 @@ export const Example: FC = () => {
 
     return (
         <div>
-            <Container hideSourceOnDrag={hideSourceOnDrag} />
-            <p>
-                <label htmlFor="hideSourceOnDrag">
-                    <input
-                        id="hideSourceOnDrag"
-                        type="checkbox"
-                        role="checkbox"
-                        checked={hideSourceOnDrag}
-                        onChange={toggle}
-                    />
-                    <small>Hide the source item while dragging</small>
-                </label>
-            </p>
+            <div>
+                <Container
+                    hideSourceOnDrag={hideSourceOnDrag}
+                    places={PLACES}
+                />
+            </div>
+            <div
+                style={{
+                    position: "absolute",
+                    top: 750
+                }}
+            >
+                <p>
+                    <label htmlFor="hideSourceOnDrag">
+                        <input
+                            id="hideSourceOnDrag"
+                            type="checkbox"
+                            role="checkbox"
+                            checked={hideSourceOnDrag}
+                            onChange={toggle}
+                        />
+                        <small>Hide the source item while dragging</small>
+                    </label>
+                </p>
+            </div>
         </div>
     );
 };
