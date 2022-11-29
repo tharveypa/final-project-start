84f8cccebf64207fa7a35050fb9403577098786f
Justin <jdclav@udel.edu>
2022-11-29 16:45:18 -0500

    moved tileList to own file

---
+++ src/images/tileList.ts
@@ -0,0 +1,244 @@
+import { tileItem } from "../interfaces";
+const tileList: tileItem[] = [
+    {
+        id: -1,
+        position: [0, 0],
+        color: "red",
+        tags: ["inside", "furniture"],
+        snap: "snap",
+        src: "./images/barrel.jpg"
+    },
+    {
+        id: -2,
+        position: [0, 0],
+        color: "green",
+        tags: ["inside", "furniture"],
+        snap: "snap",
+        src: "./images/woodFloor.jpg"
+    },
+    {
+        id: -3,
+        position: [0, 0],
+        color: "yellow",
+        tags: ["inside", "outside", "food"],
+        snap: "snap",
+        src: "./images/burger.jpg"
+    },
+    {
+        id: -4,
+        position: [0, 0],
+        color: "blue",
+        tags: ["outside", "plants", "road"],
+        snap: "snap",
+        src: "./images/bushes.jpg"
+    },
+    {
+        id: -5,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["inside", "light"],
+        snap: "snap",
+        src: "./images/candle.jpg"
+    },
+    {
+        id: -6,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["inside", "furniture"],
+        snap: "snap",
+        src: "./images/window.jpg"
+    },
+    {
+        id: -7,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["inside", "furniture"],
+        snap: "snap",
+        src: "./images/chair.jpg"
+    },
+    {
+        id: -8,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["inside", "outside", "storage"],
+        snap: "snap",
+        src: "./images/chest.jpg"
+    },
+    {
+        id: -9,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["outside", "inside", "plants"],
+        snap: "snap",
+        src: "./images/Chrysanthemum.jpg"
+    },
+    {
+        id: -10,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["outside", "cracks"],
+        snap: "snap",
+        src: "./images/cracks.jpg"
+    },
+    {
+        id: -11,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["inside", "outside", "furniture"],
+        snap: "snap",
+        src: "./images/crate.jpg"
+    },
+    {
+        id: -12,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["outside", "plants"],
+        snap: "snap",
+        src: "./images/tree.jpg"
+    },
+    {
+        id: -13,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["inside", "outside", "furniture"],
+        snap: "snap",
+        src: "./images/door.jpg"
+    },
+    {
+        id: -14,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["outside", "decoration"],
+        snap: "snap",
+        src: "./images/flag.jpg"
+    },
+    {
+        id: -15,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["inside", "furniture"],
+        snap: "snap",
+        src: "./images/floor.jpg"
+    },
+    {
+        id: -16,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["inside", "outside", "coin"],
+        snap: "snap",
+        src: "./images/goldCoin.jpg"
+    },
+    {
+        id: -17,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["outside", "plants", "road"],
+        snap: "snap",
+        src: "./images/grass.jpg"
+    },
+    {
+        id: -18,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["outside", "plants"],
+        snap: "snap",
+        src: "./images/greenTree.jpg"
+    },
+    {
+        id: -19,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["outside", "stone"],
+        snap: "snap",
+        src: "./images/hugeStone.jpg"
+    },
+    {
+        id: -20,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["inside", "item"],
+        snap: "snap",
+        src: "./images/paper.jpg"
+    },
+    {
+        id: -21,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["inside", "outside", "food"],
+        snap: "snap",
+        src: "./images/pasta.jpg"
+    },
+    {
+        id: -22,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["outside", "road"],
+        snap: "snap",
+        src: "./images/path.jpg"
+    },
+    {
+        id: -23,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["inside", "item"],
+        snap: "snap",
+        src: "./images/pen.jpg"
+    },
+    {
+        id: -24,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["inside", "outside", "furniture"],
+        snap: "snap",
+        src: "./images/pillar.jpg"
+    },
+    {
+        id: -25,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["inside", "outside", "food"],
+        snap: "snap",
+        src: "./images/pizza.jpg"
+    },
+    {
+        id: -26,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["inside", "outside", "plants"],
+        snap: "snap",
+        src: "./images/redFlowers.jpg"
+    },
+    {
+        id: -27,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["outside", "stone"],
+        snap: "snap",
+        src: "./images/rocks.png"
+    },
+    {
+        id: -28,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["outside", "stone"],
+        snap: "snap",
+        src: "./images/stone.jpg"
+    },
+    {
+        id: -29,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["inside", "outside", "furniture"],
+        snap: "snap",
+        src: "./images/table.jpg"
+    },
+    {
+        id: -30,
+        position: [-100, -100],
+        color: "cyan",
+        tags: ["outside", "road"],
+        snap: "snap",
+        src: "./images/topPath.jpg"
+    }
+];
+export default tileList;
