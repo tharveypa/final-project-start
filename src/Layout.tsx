9dbe958412932551e22041c5feeb129c88c8025b
imjackson <development@jacksongpack.com>
2022-11-29 16:55:22 -0500

    task: implement drag and drop from left menu

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,20 +1,67 @@
 /* eslint-disable no-extra-parens */
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Container, Row, Button, Col } from "react-bootstrap";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
+import type { Furniture } from "./components/types";
+
 import FurnitureList from "./components/FurnitureList";
+import RoomBoard from "./components/RoomBoard";
 
 import "./styles/Layout.css";
 import "./styles/index.css";
-import RoomBoard from "./components/RoomBoard";
 
 // const roomImages: string[] = ["empty", "z-style", "suite-style"];
 
 const Layout = () => {
     const [roomChoicesShown, setRoomChoiceVisibility] = useState(false);
     // const [roomChoice, setRoomChoice] = useState("empty");
+
+    const [furnitureInRoomBoard, setFurnitureInRoomBoard] = useState<
+        Furniture[]
+    >([]);
+
+    const addToRoomBoard = (item: Furniture, left: number, top: number) => {
+        console.log("ADD TO ROOM BOARD");
+        console.log("ID:", item.id);
+        console.log("HEIGHT:", item.height);
+        console.log("WIDTH:", item.width);
+        console.log("TOP:", top);
+        console.log("LEFT:", left);
+        const newId = `${item.name}${furnitureInRoomBoard.length + 1}`;
+        const newItem = { ...item, id: newId, left: left, top: top };
+        const newList = [...furnitureInRoomBoard, newItem];
+        setFurnitureInRoomBoard(newList);
+    };
+
+    const clearRoomBoard = () => {
+        console.log("CLEARING ROOM");
+        setFurnitureInRoomBoard([]);
+    };
+
+    const moveFurniture = useCallback(
+        (id, left, top) => {
+            console.log("MOVE FURNITURE");
+            const newItems = furnitureInRoomBoard.map((item) => {
+                console.log("ITEM:", item.id);
+                return item.id === id
+                    ? { ...item, top: top, left: left }
+                    : item;
+            });
+            console.log("ID:", id);
+            console.log("TOP:", top);
+            console.log("LEFT:", left);
+            console.log(newItems);
+            setFurnitureInRoomBoard(newItems);
+        },
+        [furnitureInRoomBoard]
+    );
+
+    useEffect(() => {
+        console.log("objects");
+        console.log(furnitureInRoomBoard);
+    }, [furnitureInRoomBoard]);
 
     return (
         <DndProvider backend={HTML5Backend}>
@@ -32,7 +79,9 @@ const Layout = () => {
                 >
                     <Container>
                         <Row>
-                            <Button>Clear Room</Button>
+                            <Button onClick={() => clearRoomBoard()}>
+                                Clear Room
+                            </Button>
                         </Row>
                         <Row>
                             <div
@@ -97,7 +146,11 @@ const Layout = () => {
                     </Container>
                 </div>
                 <div id="main-board-container">
-                    <RoomBoard>{null}</RoomBoard>
+                    <RoomBoard
+                        furnitureInRoomBoard={furnitureInRoomBoard}
+                        moveFurniture={moveFurniture}
+                        addToRoomBoard={addToRoomBoard}
+                    />
                 </div>
             </div>
         </DndProvider>
