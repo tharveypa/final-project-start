06fd5dea717ba50ac03ba0a6add8c05d4aac58d7
JeanaeClark <jeclark@udel.edu>
2022-11-29 20:26:48 -0500

    working on adding multiple images plus style changes

--- src/index.tsx
+++ src/index.tsx
@@ -11,8 +11,8 @@ observe((picPosition: [number, number]) => {
         <React.StrictMode>
             <div
                 style={{
-                    width: "250px",
-                    height: "500px",
+                    width: "100%",
+                    height: "400px",
                     border: "1px solid gray"
                 }}
             >
