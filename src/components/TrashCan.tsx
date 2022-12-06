8df1e036b3be8a51f036d35c6067272ad9750d38
bladetyr <bladetyr@udel.edu>
2022-12-06 10:51:16 -0500

    TWO trash cans?!?!?

--- src/components/TrashCan.tsx
+++ src/components/TrashCan.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDrop } from "react-dnd";
 import { ItemTypes } from "../constants";
-import { cardData } from "../interfaces/cardData";
+import { Task } from "../interfaces/task";
 import trash from "../trash.png";
 
 type deleteCardProps = {
@@ -10,12 +10,14 @@ type deleteCardProps = {
 
 const DeleteItem: React.FC<deleteCardProps> = (props) => {
     //Handles the dropping of things onto the trash can
-    const [{ item }, drop] = useDrop({
+    const [, drop] = useDrop({
         accept: [ItemTypes.Card],
-        collect: (monitor) => ({
-            item: monitor.getItem()
-        }),
-        drop: () => props.deleteNote(item.note.id)
+        drop: (item: {
+            type: string;
+            id: number;
+            shouldAdd: true;
+            task: Task;
+        }) => props.deleteNote(item.id)
     });
     return (
         <div ref={drop}>
