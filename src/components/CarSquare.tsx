18108bf0f56915396b9b8354ddce06905c5f8e71
emba00 <emba@udel.edu>
2022-12-08 23:26:15 -0500

    working through different drop types

--- src/components/CarSquare.tsx
+++ src/components/CarSquare.tsx
@@ -14,46 +14,203 @@ type CarSquareProps = {
 
 const CarSquare: React.FC<CarSquareProps> = (props) => {
     const { pic, left, top, title, description } = props;
-    const [{ isDragging }, drag] = useDrag({
-        item: {
-            type: ItemTypes.PIC,
-            pic: pic,
-            top: top,
-            left: left,
-            title: title,
-            description: description
-        },
-        collect: (monitor: DragSourceMonitor) => ({
-            isDragging: !!monitor.isDragging()
-        })
-    });
-
-    return (
-        <span className="getinlineplz">
-            <span
-                ref={drag}
-                style={{
-                    position: "relative",
-                    padding: "0.5 rem 1rem",
-                    opacity: isDragging ? 0.5 : 1,
-                    fontSize: 10,
-                    fontWeight: "bold",
-                    cursor: "move",
-                    textAlign: "center",
-                    left: left,
-                    top: top
-                }}
-            >
-                {/* <header>{pic}</header> */}
-                <img
-                    src={require("./images/" + pic + ".png")}
-                    width="50"
-                    height="50"
-                />
+    //let drag, isDragging;
+    if (
+        description === "BrownDirt" ||
+        description === "BlackDirt" ||
+        description === "TanDirt" ||
+        description === "WhitePoop" ||
+        description === "BrownPoop" ||
+        description === "GreenPoop" ||
+        description === "BreakWindows" ||
+        description === "WipeCar"
+    ) {
+        const [{ isDragging }, drag] = useDrag({
+            item: {
+                type: ItemTypes.DROPS,
+                pic: pic,
+                top: top,
+                left: left,
+                title: title,
+                description: description
+            },
+            collect: (monitor: DragSourceMonitor) => ({
+                isDragging: !!monitor.isDragging()
+            })
+        });
+        return (
+            <span className="getinlineplz">
+                <span
+                    ref={drag}
+                    style={{
+                        position: "relative",
+                        padding: "0.5 rem 1rem",
+                        opacity: isDragging ? 0.5 : 1,
+                        fontSize: 10,
+                        fontWeight: "bold",
+                        cursor: "move",
+                        textAlign: "center",
+                        left: left,
+                        top: top
+                    }}
+                >
+                    {/* <header>{pic}</header> */}
+                    <img
+                        src={require("./images/" + pic + ".png")}
+                        width="50"
+                        height="50"
+                    />
+                </span>
+                {pic}
             </span>
-            {pic}
-        </span>
-    );
+        );
+    } else if (
+        description === "City" ||
+        description === "Forest" ||
+        description === "Street" ||
+        description === "Dump" ||
+        description === "Mountain" ||
+        description === "Alley" ||
+        description === "Cave" ||
+        description === "Garage"
+    ) {
+        const [{ isDragging }, drag] = useDrag({
+            item: {
+                type: ItemTypes.BACKS,
+                pic: pic,
+                top: top,
+                left: left,
+                title: title,
+                description: description
+            },
+            collect: (monitor: DragSourceMonitor) => ({
+                isDragging: !!monitor.isDragging()
+            })
+        });
+        return (
+            <span className="getinlineplz">
+                <span
+                    ref={drag}
+                    style={{
+                        position: "relative",
+                        padding: "0.5 rem 1rem",
+                        opacity: isDragging ? 0.5 : 1,
+                        fontSize: 10,
+                        fontWeight: "bold",
+                        cursor: "move",
+                        textAlign: "center",
+                        left: left,
+                        top: top
+                    }}
+                >
+                    {/* <header>{pic}</header> */}
+                    <img
+                        src={require("./images/" + pic + ".png")}
+                        width="50"
+                        height="50"
+                    />
+                </span>
+                {pic}
+            </span>
+        );
+    } else if (
+        description === "Wing" ||
+        description === "ShineCar" ||
+        description === "LightTint" ||
+        description === "MediumTint" ||
+        description === "DarkTint"
+    ) {
+        const [{ isDragging }, drag] = useDrag({
+            item: {
+                type: ItemTypes.ONEPOS,
+                pic: pic,
+                top: top,
+                left: left,
+                title: title,
+                description: description
+            },
+            collect: (monitor: DragSourceMonitor) => ({
+                isDragging: !!monitor.isDragging()
+            })
+        });
+        return (
+            <span className="getinlineplz">
+                <span
+                    ref={drag}
+                    style={{
+                        position: "relative",
+                        padding: "0.5 rem 1rem",
+                        opacity: isDragging ? 0.5 : 1,
+                        fontSize: 10,
+                        fontWeight: "bold",
+                        cursor: "move",
+                        textAlign: "center",
+                        left: left,
+                        top: top
+                    }}
+                >
+                    {/* <header>{pic}</header> */}
+                    <img
+                        src={require("./images/" + pic + ".png")}
+                        width="50"
+                        height="50"
+                    />
+                </span>
+                {pic}
+            </span>
+        );
+    } else if (
+        description === "Red" ||
+        description === "Blue" ||
+        description === "Green" ||
+        description === "Yellow" ||
+        description === "Purple" ||
+        description === "Orange" ||
+        description === "SlashTires" ||
+        description === "BreakWindows" ||
+        description === "FillTires" ||
+        description === "RepairWindows"
+    ) {
+        const [{ isDragging }, drag] = useDrag({
+            item: {
+                type: ItemTypes.CARS,
+                pic: pic,
+                top: top,
+                left: left,
+                title: title,
+                description: description
+            },
+            collect: (monitor: DragSourceMonitor) => ({
+                isDragging: !!monitor.isDragging()
+            })
+        });
+        return (
+            <span className="getinlineplz">
+                <span
+                    ref={drag}
+                    style={{
+                        position: "relative",
+                        padding: "0.5 rem 1rem",
+                        opacity: isDragging ? 0.5 : 1,
+                        fontSize: 10,
+                        fontWeight: "bold",
+                        cursor: "move",
+                        textAlign: "center",
+                        left: left,
+                        top: top
+                    }}
+                >
+                    {/* <header>{pic}</header> */}
+                    <img
+                        src={require("./images/" + pic + ".png")}
+                        width="50"
+                        height="50"
+                    />
+                </span>
+                {pic}
+            </span>
+        );
+    }
+    return <span className="getinlineplz"></span>;
 };
-
 export default CarSquare;
